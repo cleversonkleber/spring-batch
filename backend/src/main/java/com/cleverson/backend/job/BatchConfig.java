4b33e81227658d294ab5c13971da119ab903d6c3
cleversonkleber <cleversonkleber@gmail.com>
2024-05-15 22:40:33 -0300

    Update BatchConfig.java

--- backend/src/main/java/com/cleverson/backend/job/BatchConfig.java
+++ backend/src/main/java/com/cleverson/backend/job/BatchConfig.java
@@ -9,12 +9,10 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.launch.support.TaskExecutorJobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
@@ -22,12 +20,11 @@ import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.transform.Range;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.web.ServerProperties.Tomcat.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
-
 import com.cleverson.backend.entity.TipoTransacao;
 import com.cleverson.backend.entity.Transacao;
 import com.cleverson.backend.entity.TransacaoCNAB;
@@ -35,10 +32,8 @@ import com.cleverson.backend.entity.TransacaoCNAB;
 @Configuration
 public class BatchConfig {
 
-    @SuppressWarnings("unused")
-    private static final JobLauncher JobLauncher = null;
-    private PlatformTransactionManager transactionManager;
-    private JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+    private final JobRepository jobRepository;
 
     public BatchConfig(PlatformTransactionManager transactionManager, JobRepository jobRepository) {
         this.transactionManager = transactionManager;
@@ -49,61 +44,54 @@ public class BatchConfig {
     Job job(Step step) {
         return new JobBuilder("job", jobRepository)
                 .start(step)
-                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
     @Bean
     Step step(
-            ItemReader<TransacaoCNAB> reader,
-            ItemProcessor<TransacaoCNAB, Transacao> prossor,
+            FlatFileItemReader<TransacaoCNAB> reader,
+            ItemProcessor<TransacaoCNAB, Transacao> processor,
             ItemWriter<Transacao> writer) {
         return new StepBuilder("step", jobRepository)
                 .<TransacaoCNAB, Transacao>chunk(1000, transactionManager)
                 .reader(reader)
-                .processor(prossor)
+                .processor(processor)
                 .writer(writer)
                 .build();
     }
 
     @StepScope
     @Bean
-    FlatFileItemReader<TransacaoCNAB> reader(@Value("#{jobParameters['cnabFile']}") Resource resource) {
+    FlatFileItemReader<TransacaoCNAB> reader(
+            @Value("#{jobParameters['cnabFile']}") Resource resource) {
         return new FlatFileItemReaderBuilder<TransacaoCNAB>()
                 .name("reader")
-                // .resource(new FileSystemResource(
-                // "/home/cjcleverson/Desenvolvimento/Spring-boot/spring-batch/backend/files/CNAB.txt"))
+                .resource(resource)
                 .fixedLength()
-                .columns(new Range(1, 1), new Range(2, 9),
-                        new Range(10, 19), new Range(20, 30),
-                        new Range(31, 42), new Range(43, 48),
-                        new Range(49, 62), new Range(63, 80))
+                .columns(new Range(1, 1), new Range(2, 9), new Range(10, 19), new Range(20, 30),
+                        new Range(31, 42), new Range(43, 48), new Range(49, 62), new Range(63, 80))
                 .names("tipo", "data", "valor", "cpf", "cartao", "hora", "donoDaLoja", "nomeDaLoja")
                 .targetType(TransacaoCNAB.class)
                 .build();
-
     }
 
     @Bean
     ItemProcessor<TransacaoCNAB, Transacao> processor() {
-        // Wither pattern
         return item -> {
             var tipoTransacao = TipoTransacao.findByTipo(item.tipo());
-            var valornOrmalizado = item.valor()
+            var valorNormalizado = item.valor()
                     .divide(new BigDecimal(100))
                     .multiply(tipoTransacao.getSinal());
 
-            return new Transacao(
-                    null,
-                    item.tipo(), null,
-                    valornOrmalizado,
-                    item.cpf(),
-                    item.cartao(), null,
-                    item.donoDaLoja().trim(),
-                    item.nomeDaLoja().trim())
-                    .withValor(item.valor().divide(BigDecimal.valueOf(100)))
+            var transacao = new Transacao(
+                    null, item.tipo(), null,
+                    valorNormalizado,
+                    item.cpf(), item.cartao(), null,
+                    item.donoDaLoja().trim(), item.nomeDaLoja().trim())
                     .withData(item.data())
                     .withHora(item.hora());
+
+            return transacao;
         };
     }
 
@@ -112,11 +100,13 @@ public class BatchConfig {
         return new JdbcBatchItemWriterBuilder<Transacao>()
                 .dataSource(dataSource)
                 .sql("""
-                        insert into transacao (
-                            tipo,data,valor, cpf, cartao,hora, dono_loja, nome_loja
-                        )values(
-                            :tipo, :data, :valor, :cpf, :cartao, :hora, :donoDaLoja, :nomeDaLoja
-                            )
+                          INSERT INTO transacao (
+                            tipo, data, valor, cpf, cartao,
+                            hora, dono_loja, nome_loja
+                          ) VALUES (
+                            :tipo, :data, :valor, :cpf, :cartao,
+                            :hora, :donoDaLoja, :nomeDaLoja
+                          )
                         """)
                 .beanMapped()
                 .build();
@@ -130,5 +120,4 @@ public class BatchConfig {
         jobLauncher.afterPropertiesSet();
         return jobLauncher;
     }
-
 }
