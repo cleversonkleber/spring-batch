e66ddea496b33cf95a871864a826daea9c4b8a33
cleversonkleber <cleversonkleber@gmail.com>
2024-05-15 22:50:17 -0300

    Update TransacaoReport.java

--- backend/src/main/java/com/cleverson/backend/entity/TransacaoReport.java
+++ backend/src/main/java/com/cleverson/backend/entity/TransacaoReport.java
@@ -8,13 +8,18 @@ public record TransacaoReport(
         String nome,
         List<Transacao> transacoes) {
 
-    public TransacaoReport addtotal(BigDecimal valor) {
-        return new TransacaoReport(this.total().add(valor), this.nome(), this.transacoes());
-    }
+  public TransacaoReport addTotal(BigDecimal valor) {
+    return new TransacaoReport(this.total().add(valor), this.nome, this.transacoes);
+  }
 
-    public TransacaoReport addTransacao(Transacao transacao) {
-        transacoes.add(transacao);
-        return new TransacaoReport(total, nome, transacoes);
-    }
+  public TransacaoReport addTransacao(Transacao transacao) {
+    var transacoes = this.transacoes();
+    transacoes.add(transacao);
+    return new TransacaoReport(this.total(), this.nome, transacoes);
+  }
+
+  public TransacaoReport withNomeDaLoja(String nomeDaLoja) {
+    return new TransacaoReport(this.total(), nome, this.transacoes);
+  }
 
 }
